6bb807f72f10cf71079c93a850a08add3c5265f5
AbhinavSingh-create <as2804178@gmail.com>
2024-11-16 00:24:47 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,5 @@
+let submit=document.querySelector('.btn-primary')
+
+submit.addEventListener('click',()=>{
+    location.href='last.html'
+})
---
+++ index.js
@@ -0,0 +1,10 @@
+document.querySelector('.jerk')
+.addEventListener('click',()=>{
+    let popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+})
+document.querySelector('.abhi')
+.addEventListener('click',()=>{
+    let pop=document.getElementById('my-finger')
+    pop.classList.toggle('hide')
+})
